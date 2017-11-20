445aa44e533d9e8a3ef2784fe9f2347814efb0f3
jonepl <jonepl.ufl@gmail.com>
2017-11-19 22:46:34 -0500

    Minor Tweak

--- src/main/RemoteStart/StartRemotePeers.java
+++ src/main/RemoteStart/StartRemotePeers.java
@@ -70,7 +70,7 @@ public class StartRemotePeers {
 
                 // *********************** IMPORTANT *************************** //
                 // If your program is JAVA, use this line.
-                //System.out.println("ssh " + pInfo.peerAddress + " cd " + path + "; ./peerProcess " + pInfo.peerId);
+                       //System.out.println("ssh " + pInfo.peerAddress + " cd " + path + "; java peerProcess " + pInfo.peerId);
                 Runtime.getRuntime().exec("ssh " + pInfo.peerAddress + " cd " + path + "; java peerProcess " + pInfo.peerId);
 
                 // If your program is C/C++, use this line instead of the above line.
